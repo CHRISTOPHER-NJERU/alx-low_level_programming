7f522f0a0529f7351070c5bb257913d74bbdb40a
CHRISTOPHER-NJERU <chriskagwiri@gmail.com>
2023-07-14 18:34:04 -0700

    Write a program that prints the alphabet in lowercase, followed by a new line.

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,8 +11,9 @@
 int main(void)
 {
 	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i;	
-	for (i = 0; i <= 26; i++);
+	int i;
+
+	for (i = 0; i <= 26; i++)
 	{
 		putchar(alphabet[i]);
 	}
