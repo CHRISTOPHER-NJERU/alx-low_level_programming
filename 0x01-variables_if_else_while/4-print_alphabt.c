2dda9eb9e3635254f19164b528710e1dbea01eea
CHRISTOPHER-NJERU <chriskagwiri@gmail.com>
2023-07-15 03:56:07 -0700

    Print lowercase alphabets but not q and e.

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -14,18 +14,18 @@ int main(void)
 {
 	int lowercase = 'a';
 	
-		while (lowercase <= 'z')
+	while (lowercase <= 'z')
+	{
+		if (lowercase == 'e' || lowercase == 'q')
 		{
-			if (lowercase == 'e' || lowercase == 'q')
-			{
-				lowercase += 1;
-			}
-			else
-			{
-				putchar (lowercase);
-				lowercase += 1;
-			}
+			lowercase += 1;
 		}
+		else
+		{
+			putchar (lowercase);
+			lowercase += 1;
+		}
+	}
 	putchar ('\n');
 	return (0);
 }
