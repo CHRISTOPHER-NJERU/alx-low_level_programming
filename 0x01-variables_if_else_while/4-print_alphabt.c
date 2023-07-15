dd9263363834ea08dbdd4ba568d7c4304cc7b04b
CHRISTOPHER-NJERU <chriskagwiri@gmail.com>
2023-07-15 10:02:29 -0700

    Alphabets lowercase.

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -13,19 +13,19 @@
 int main(void)
 {
 	int lowercase = 'a';
-
+	
 	while (lowercase <= 'z')
+	{
+		if (lowercase == 'e' || lowercase == 'q')
 		{
-			if (lowercase == 'e' || lowercase == 'q')
-			{
-				lowercase += 1;
-			}
-			else
-			{
-				putchar (lowercase);
-				lowercase += 1;
-			}
+			lowercase += 1;
 		}
+		else
+		{
+			putchar (lowercase);
+			lowercase += 1;
+		}
+	}
 	putchar ('\n');
 	return (0);
 }
