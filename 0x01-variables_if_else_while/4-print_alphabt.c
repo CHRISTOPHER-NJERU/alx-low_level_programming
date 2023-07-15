c620812196a1f3e15d00e318820f8572ff76bc04
CHRISTOPHER-NJERU <chriskagwiri@gmail.com>
2023-07-14 19:50:17 -0700

    Print lowercase alphabets with exception of e and q.

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Print lowercase alphabets with exception of q and e.
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+        int lowercase = 'a';
+        while (lowercase <= 'z')
+        {
+	if (lowercase == 'e' || lowercase == 'q')
+	{
+                lowercase += 1;
+        }
+	else 
+	{
+		putchar (lowercase);
+		lowercase += 1;
+	}
+	}
+        putchar ('\n');
+        return (0);
+}
