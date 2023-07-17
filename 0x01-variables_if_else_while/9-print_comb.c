c4fa272016336aea9d19ae615898036ac0b1b405
CHRISTOPHER-NJERU <chriskagwiri@gmail.com>
2023-07-17 02:19:07 -0700

    Write a program that prints all possible combinations of single-digit numbers.

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,15 +14,15 @@ int main(void)
 	int n;
 	
 	for (n = 48; n <= 57; n++)
-		{
-			putchar(n);
+	{
+		putchar(n);
 		if (n == 57)
 		{
 			continue;
 		}
 		putchar(',');
 		putchar(' ');
-		}
+	}
 	putchar('\n');
 	return (0);
 }
