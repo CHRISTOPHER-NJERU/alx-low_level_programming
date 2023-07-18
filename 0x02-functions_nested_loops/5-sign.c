526730f712cef23dc948eb9c7931bb43eb83dffe
CHRISTOPHER-NJERU <chriskagwiri@gmail.com>
2023-07-18 14:34:28 -0700

    Write a function that prints the sign of a number.

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_sign - Print signs and determine if input
+ * is geater, equal to or zero.
+ *
+ * @n: The input number of an integer.
+ *
+ * Return: (1) greater than zero, (0) equal to zero,
+ * (-1) less than zero
+ *
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
