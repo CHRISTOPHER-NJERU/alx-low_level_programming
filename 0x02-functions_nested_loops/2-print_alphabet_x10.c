0fd25b8637e4b8624bf2da843e44de468c948916
CHRISTOPHER-NJERU <chriskagwiri@gmail.com>
2023-07-18 12:19:20 -0700

    Print alphabets lowercase 10 times.

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,12 +11,12 @@ void print_alphabet_x10(void)
 	int num;
 	int letter;
 
-	for (num = 0; num <= 10; num++)
+	for (num = 0; num < 10; num++)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 		{
 			_putchar(letter);
-			_putchar('\n');
 		}
+		_putchar('\n');
 	}
 }
