77e2f4f8e12a146cb3c17ed40733614bcae5d8a0
CHRISTOPHER-NJERU <chriskagwiri@gmail.com>
2023-07-19 04:40:13 -0700

    Times table for 9.

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,34 +9,36 @@
 
 void times_table(void)
 {
-	int i, j, k;
+	int i, j, k, l, m;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			k = i * j;
-			if (j == 0)
+			k = j * i;
+			if (k > 9)
 			{
-				_putchar(k + 'o');
+				l = k % 10;
+				m = (k - l) / 10;
+				_putchar(',');
+				_putchar(' ');
+				_putchar(m + '0');
+				_putchar(l + '0');
 			}
-			if (k <= 9 && j != 0)
+			else
+			{
+			if (j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-				_putchar(k + 'o');
 			}
-			else if (k >= 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((k / 10) + 'o');
-				_putchar((k % 10) + 'o');
+			_putchar(k + '0');
 			}
 		}
 		_putchar('\n');
 	}
 }
+
 
 
