42bde704f2c234502942b51c9d5a6a7b6716c903
CHRISTOPHER-NJERU <chriskagwiri@gmail.com>
2023-07-22 10:42:25 -0700

    FizzBuzz Test.

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,10 +16,10 @@ int main(void)
 	{
 		if (i % 15 == 0)
 			printf("FizzBuzz");
-		else if (i % 5 == 0)
-			printf("Buzz");
 		else if (i % 3 == 0)
 			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
 		else
 			printf("%i", i);
 		if (i < 100)
