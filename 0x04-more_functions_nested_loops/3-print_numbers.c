0300d8b4c8823d3bcbc659f7e00a50d920f14aad
CHRISTOPHER-NJERU <chriskagwiri@gmail.com>
2023-07-22 07:35:50 -0700

    Addins int _putchar(char c). In main.h file.

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,14 +3,14 @@
 /**
  * print_numbers - Prints numbers from 0 to 9.
  *
- * Return: (0) success.
+ * Return: void.
  *
  */
 
 void print_numbers(void)
 {
 	char c;
-	
+
 	for (c = '0'; c <= '9'; c++)
 	{
 		_putchar(c);
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal.
+ *
+ *@n: number of times to print char(_).
+ *
+ *Return: line.
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	putchar('\n');
+}
