555b0d59ef0de0e46aa1b6688d2045ac0bf096f7
CHRISTOPHER-NJERU <chriskagwiri@gmail.com>
2023-07-28 10:54:10 -0700

    Write a function that capitalizes all words of a string.

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,22 +11,30 @@
 
 char *cap_string(char *str)
 {
-	int i, j;
-	char c[] = {44, 59, 46, '!', '?', '"', '(', ')', '{', '}', ' ', '\t', '\n'};
+	int i = 0;
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (str[i])
 	{
-		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
-		{
-			str[i] = str[i] - 32;
-		}
-		for (j = 0; c[j] != '\0'; j++)
-		{
-			if (c[j] != str[i] && (str[i + 1] >= 'a' && str[i + 1] <= 'z'))
-			{
-				str[i] = str[i + 1] - 32;
-			}
-		}
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+
+			i++;
+
+		if (str[i - 1] == ' ' ||
+		str[i - 1] == '\t' ||
+		str[i - 1] == '\n' ||
+		str[i - 1] == ',' ||
+		str[i - 1] == ';' ||
+		str[i - 1] == '.' ||
+		str[i - 1] == '!' ||
+		str[i - 1] == '?' ||
+		str[i - 1] == '"' ||
+		str[i - 1] == '(' ||
+		str[i - 1] == ')' ||
+		str[i - 1] == '{' ||
+		str[i - 1] == '}' ||
+		i == 0)
+			str[i] -= 32;
+		i++;
 	}
 	return (str);
 }
